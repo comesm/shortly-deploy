2cac4b368a3e7b96074fe891e866d072a90d8796
comesm <comesm@gmail.com>
2017-01-18 21:11:00 -0800

    blaah

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var app = require('./server-config.js');
 
 var port = 4568;
-///root/hrsf52-shortly-deploy
+///root//hrsf52-shortly-deploy
 app.listen(port);
 
 console.log('Server now listening on poert ' + port);
